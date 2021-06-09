6686fd426c179554768c5506dedd320f048aa440
maxfrazier <frazier.maxwell.t@gmail.com>
2021-06-09 13:39:27 -0400

    Updates
    Refactored Tennis Code

--- csharp/Tennis/TennisGame.cs
+++ csharp/Tennis/TennisGame.cs
@@ -1,33 +1,68 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Tennis
 {
+    class TennisPlayers
+    {
+        public string player1Name;
+        public string player2Name;
+        public int player1Score;
+        public int player2Score;
+   
+        /// <summary>
+        /// Constructor for the TennisPlayers object.
+        /// </summary>
+        /// <param name="tp1Name">Name of tennis player 1.</param>
+        /// <param name="tp2Name">Name of tennis player 2.</param>
+        public TennisPlayers(string tp1Name, string tp2Name)
+        {
+            player1Name = tp1Name;
+            player2Name = tp2Name;
+        }
+    }
+
     class TennisGame : ITennisGame
     {
-        private int m_score1 = 0;
-        private int m_score2 = 0;
-        private string player1Name;
-        private string player2Name;
+        private TennisPlayers players;
 
+        /// <summary>
+        /// Begins a game of tennis by instantiating the TennisPlayers object.
+        /// </summary>
+        /// <param name="player1Name">Name of player 1.</param>
+        /// <param name="player2Name">Name of player 2.</param>
         public TennisGame(string player1Name, string player2Name)
         {
-            this.player1Name = player1Name;
-            this.player2Name = player2Name;
+            players = new TennisPlayers(player1Name, player2Name);
         }
 
+        /// <summary>
+        /// Updates the score using the name of the player who scored.
+        /// </summary>
+        /// <param name="playerName">Name of scoring player.</param>
         public void WonPoint(string playerName)
         {
-            if (playerName == "player1")
-                m_score1 += 1;
+            if(playerName == players.player1Name)
+            {
+                players.player1Score += 1;
+            } 
             else
-                m_score2 += 1;
+            {
+                players.player2Score += 1;
+            }
         }
 
+        /// <summary>
+        /// Gets the score of the Tennis Game.
+        /// </summary>
+        /// <returns>The current score of the tennis game.</returns>
         public string GetScore()
         {
             string score = "";
             var tempScore = 0;
-            if (m_score1 == m_score2)
+            if (players.player1Score == players.player2Score)
             {
-                switch (m_score1)
+                switch (players.player1Score)
                 {
                     case 0:
                         score = "Love-All";
@@ -44,20 +79,20 @@ namespace Tennis
 
                 }
             }
-            else if (m_score1 >= 4 || m_score2 >= 4)
+            else if (players.player1Score >= 4 || players.player2Score >= 4)
             {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
+                var difference = players.player1Score - players.player2Score;
+                if (difference == 1) score = "Advantage player1";
+                else if (difference == -1) score = "Advantage player2";
+                else if (difference >= 2) score = "Win for player1";
                 else score = "Win for player2";
             }
             else
             {
                 for (var i = 1; i < 3; i++)
                 {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
+                    if (i == 1) tempScore = players.player1Score;
+                    else { score += "-"; tempScore = players.player2Score; }
                     switch (tempScore)
                     {
                         case 0:
